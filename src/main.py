beb429f3c906370e2145410e4f08d12af23f53d8
236694 Wielgus Michal <236694@edu.p.lodz.pl>
2022-03-24 00:06:52 +0100

    now its technically a solver

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from src.board import Board
 
 
 def is_solved(board):
-    return board == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    return board.tiles == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0]
 
 
 if __name__ == '__main__':
@@ -12,13 +12,10 @@ if __name__ == '__main__':
     moves = []
 
     while not is_solved(board):
-        # print(board.find_legal_moves(), end='\t\t')
         move = choice(board.find_legal_moves())
-        # print(move)
 
         board.move_tile(move)
         moves.append(move)
-        board.print()
 
     print(moves)
     print(f"ilosc ruchow = {len(moves)}")
